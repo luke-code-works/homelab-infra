52feca9bfd1de56b878ff50081abcbdb5783c5f0
Luke <133242630+luke-code-works@users.noreply.github.com>
2025-07-07 15:44:29 +0200

    feat: improve network and volume naming

--- traffix/scripts/setup-networking.sh
+++ traffix/scripts/setup-networking.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker network create --driver=bridge --subnet=10.89.0.0/16 traffix
-docker network create --driver=bridge --subnet=10.172.0.0/16 --internal traffix_internal
+docker network create --driver=bridge --subnet=10.172.0.0/16 --internal traffix-internal
